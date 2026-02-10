0d64cd6146c466c3e1b6683dd7001c2c7a51d1dd
依伊 <3434947017@qq.com>
2026-02-10 17:20:18 +0800

    还剩一个开机自启？

---
+++ PopupBlocker/Controls/CardToggleSwitch.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Wpf.Ui.Controls;
+
+namespace PopupBlocker.Controls
+{
+    public class CardToggleSwitch : System.Windows.Controls.ContentControl
+    {
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                nameof(Icon),
+                typeof(IconElement),
+                typeof(CardToggleSwitch));
+
+        public object Icon
+        {
+            get => (IconElement)GetValue(IconProperty);
+            set => SetValue(IconProperty, value);
+        }
+
+        public static readonly DependencyProperty IsCheckedProperty =
+            DependencyProperty.Register(
+                nameof(IsChecked),
+                typeof(bool),
+                typeof(CardToggleSwitch),
+                new FrameworkPropertyMetadata(
+                    false,
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool IsChecked
+        {
+            get => (bool)GetValue(IsCheckedProperty);
+            set => SetValue(IsCheckedProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(
+                nameof(Command),
+                typeof(ICommand),
+                typeof(CardToggleSwitch));
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        static CardToggleSwitch()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(CardToggleSwitch),
+                new FrameworkPropertyMetadata(typeof(CardToggleSwitch)));
+        }
+    }
+}
--- PopupBlocker/Views/Pages/SettingPage.xaml.cs
+++ PopupBlocker/Views/Pages/SettingPage.xaml.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace PopupBlocker.Views.Pages
+﻿namespace PopupBlocker.Views.Pages
 {
     /// <summary>
     /// SettingPage.xaml 的交互逻辑
     /// </summary>
-    public partial class SettingPage : Page
+    public partial class SettingPage : System.Windows.Controls.Page
     {
         public SettingPage()
         {
             InitializeComponent();
+            this.DataContext = Utility.Commons.Singleton<ViewModels.SettingViewModel>.Instance;
         }
     }
 }
